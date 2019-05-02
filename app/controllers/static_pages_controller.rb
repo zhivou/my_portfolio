b94ec8d680e2a2afb2ae8e332718499171393ca7
Dmitrii Skrylev <zivou4@gmail.com>
2019-05-01 22:25:21 -0600

    Added nav_bar, footer + scss for them

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,4 @@
 class StaticPagesController < ApplicationController
-  def home
-  end
-
-  def blog
-  end
-
-  def project
-  end
-
   def contact
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/blog'
-
-  get 'static_pages/project'
-
-  get 'static_pages/contact'
-
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'blogs#index'
+  get 'contacts', to: 'static_pages#contact'
 end
