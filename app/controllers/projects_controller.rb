680c112e84ccb776b608c98f97052e2517616605
Dmitrii Skrylev <zivou4@gmail.com>
2019-05-06 21:09:18 -0600

    Uploaded pic example, changed name to title added position sorting

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,7 +4,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = Project.all
+    @projects = Project.order("position ASC")
   end
 
   # GET /projects/1
