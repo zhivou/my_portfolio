93fc5f0a63250b152356738b5fcab305255de80b
Dmitrii Skrylev <dima.skrylev@plansource.com>
2019-05-15 13:03:16 -0600

    Ajax with async mode

--- app/assets/javascripts/spacex.js
+++ app/assets/javascripts/spacex.js
@@ -7,7 +7,7 @@ class AjaxCaller {
     }).done(function(data) {
       let collection = {};
 
-      $("#totalLaunches").html(data.length);
+      $("#totalLaunches").html(`<div class="display-3">${data.length}</div>`);
 
       //
       // Counting all rockets and putting to the collection
@@ -29,8 +29,9 @@ class AjaxCaller {
       for(var p in collection) {
         if (collection.hasOwnProperty(p)) {
           console.log(p, collection[p]);
-          $("#rocketName").append(p);
-          $("#rocketCount").append(collection[p])
+          $("#rockets")
+              .append(`<span class="h2">${p}: </span>`)
+              .append(`<span class="h2">${collection[p]}</span><br>`)
         }
       }
     });
@@ -46,47 +47,58 @@ function getChartData (data) {
     });
 }
 
+var chart = function(){
+  var ctx = document.getElementById('launchHistory');
+  var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+
+      datasets: [{
+        label: 'Launches per year',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
+};
+
+function scrollTo (h) {
+  var url = location.href;
+  location.href = "#"+h;
+  history.replaceState(null,null,url);
+}
+
 $('document').ready(function() {
-    var ctx = document.getElementById('launchHistory');
-    var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        }
-    });
+  scrollTo("astronautmaPicture");
+  chart();
   AjaxCaller.get_launches()
 });
 
