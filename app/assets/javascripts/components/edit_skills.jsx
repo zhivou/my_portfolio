214127c2074aa572ffe53cd004db09cc4f700d64
Dmitrii Skrylev <dima.skrylev@plansource.com>
2019-05-19 17:55:34 -0600

    Started changing skills section to react

---
+++ app/assets/javascripts/components/edit_skills.jsx
@@ -0,0 +1,22 @@
+class EditSkillsListing extends React.Component {
+
+  constructor(props) {
+    super(props);
+  };
+
+  render() {
+    return (
+        <div>
+          <ShowSkills/>
+        </div>
+    );
+  }
+}
+
+const ShowSkills = (props) => {
+  return (
+      <div>
+        <p>g</p>
+      </div>
+  );
+};
