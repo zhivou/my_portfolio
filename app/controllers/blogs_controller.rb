fc3dcee440ac2d027456192aa072ffd34c6591eb
Dmitrii Skrylev <dima.skrylev@plansource.com>
2019-05-22 23:22:51 -0600

    Installed Active Storage, Rendered 1-many

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -69,6 +69,6 @@ class BlogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog).permit(:title, :body)
+      params.require(:blog).permit(:title, :body, images:[])
     end
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,6 @@
 class Blog < ApplicationRecord
-  has_many :pictures
+  #has_many :pictures
+  has_many_attached :images
 
   validates_presence_of :title, :body
 end
