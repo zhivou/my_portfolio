93cdfe9e8273612b80361b66a50698cffc2a81eb
Dmitrii Skrylev <dima.skrylev@plansource.com>
2019-05-23 17:48:19 -0600

    Added Direct upload, Next step is to refactor Avatar model and move it to users

--- app/controllers/avatar_controller.rb
+++ app/controllers/avatar_controller.rb
@@ -9,8 +9,19 @@ class AvatarController < ApplicationController
 
     respond_to do |format|
       json_writer(hash)
+      image
       format.html { redirect_to root_path, notice: 'Profile was successfully created' }
     end
+  end
+
+  def image
+    blob = ActiveStorage::Blob.create_after_upload!(
+        io: avatar_params[:attachments],
+        filename: avatar_params[:attachments].original_filename,
+        content_type: avatar_params[:attachments].content_type
+    )
+
+    render json: { filelink: url_for(blob) }
   end
 
   private
@@ -21,6 +32,6 @@ class AvatarController < ApplicationController
   end
 
   def avatar_params
-    params.require(:avatar).permit(:path, :name, :title, :short_about)
+    params.require(:avatar).permit(:path, :name, :title, :short_about, attachments: [])
   end
 end
