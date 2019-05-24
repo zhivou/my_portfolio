7edd9c67e65a85c8f6f9d0a29c2bc13b582311d9
Dmitrii Skrylev <dima.skrylev@plansource.com>
2019-05-24 10:33:44 -0600

    Added Sinitized Params for Devise User Model, added restriction on havining only one site owner(one user can be written into users table), Rendered Register form correctly with active storage laoder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   include DefaultPageContent
   include ProfileContent
 
   protected
+
+  ##
+  # This is for devise strong parameters
+  #
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys:
-        [
-            :username,
-            :first_name,
-            :last_name,
-            :title,
-            :about,
-            :avatar_image
-        ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+        :first_name,
+        :last_name,
+        :title,
+        :about,
+        :avatar_image
+    ])
   end
 end
--- app/controllers/avatar_controller.rb
+++ app/controllers/avatar_controller.rb
@@ -3,35 +3,4 @@ class AvatarController < ApplicationController
 
   def new
   end
-
-  def create
-    hash = avatar_params.to_hash
-
-    respond_to do |format|
-      json_writer(hash)
-      image
-      format.html { redirect_to root_path, notice: 'Profile was successfully created' }
-    end
-  end
-
-  def image
-    blob = ActiveStorage::Blob.create_after_upload!(
-        io: avatar_params[:attachments],
-        filename: avatar_params[:attachments].original_filename,
-        content_type: avatar_params[:attachments].content_type
-    )
-
-    render json: { filelink: url_for(blob) }
-  end
-
-  private
-  def json_writer(hash)
-    File.open(DATA_PATH,"w") do |f|
-      f.write(JSON.pretty_generate(hash))
-    end
-  end
-
-  def avatar_params
-    params.require(:avatar).permit(:path, :name, :title, :short_about, attachments: [])
-  end
 end
