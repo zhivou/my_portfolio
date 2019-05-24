7036502876f9b15206a1f83280a43baa21de0541
Dmitrii Skrylev <dima.skrylev@plansource.com>
2019-05-24 15:31:40 -0600

    Added some styling to Blogs

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,7 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.precompile += %w( blogs.css )
 Rails.application.config.assets.precompile += %w( spacex.css )
 Rails.application.config.assets.precompile += %w( spacex.js )
 
