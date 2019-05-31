375b51db38591fcbcb3722b7249804db652e7dc7
Dmitrii Skrylev <dima.skrylev@plansource.com>
2019-05-31 16:36:23 -0600

    First Rendered Description and Time words, added Object transformation to State

--- app/assets/javascripts/components/Blog.jsx
+++ app/assets/javascripts/components/Blog.jsx
@@ -6,43 +6,57 @@ class Blog extends React.Component {
 
   constructor(props){
     super(props);
+
     this.state = {
-      blogs: []
+       id: null,
+       title: null,
+       description: null,
+       time_words: null,
+       created_at: null,
+       updated_at: null
     };
   }
 
   componentDidMount() {
-    $.ajax({
-      method: 'GET',
-      url: 'blogs-api',
-      dataType: 'json',
-    }).done((data) => {
-          this.setState({ blogs: data })
-        })
+
+    temp_holder = Object.values(this.props);
+    this.setState({
+      id: temp_holder[0].id,
+      title: temp_holder[0].title,
+      created_at: temp_holder[0].created_at,
+      updated_at: temp_holder[0].updated_at,
+      description: temp_holder[1],
+      time_words: temp_holder[2]
+    })
+
+    // $.ajax({
+    //   method: 'GET',
+    //   url: 'blogs-api',
+    //   dataType: 'json',
+    // }).done((data) => {
+    //       this.setState({ blogs: data })
+    //     })
   }
 
   render() {
     return (
       <div>
-        <Post posts={this.state.blogs} />
+        <Post post={this.state} />
       </div>
     )
   }
 }
 
 const Post = (props) => {
-  let posts = props.posts.map((post) => {
-    link =  `/blogs/${post.id}`;
-    return(
-      <div className="card shadow p-3 mb-3 bg-white rounded">
-        <a href={link}>{post.title}</a>
-      </div>
-    )
-  });
+  link = `/blogs/${props.post.id}`;
 
   return(
     <div>
-      {posts}
+      <div className="card shadow p-3 mb-3 bg-white rounded">
+        <a href={link}>{props.post.title}</a>
+        <p>{props.post.description}</p>
+        <p>{props.post.time_words}</p>
+      </div>
     </div>
   )
 };
