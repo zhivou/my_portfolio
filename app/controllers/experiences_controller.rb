a140c1efe8764b79c85482f995b5df177fdc5575
Dmitrii Skrylev <dima.skrylev@plansource.com>
2019-07-12 10:25:04 -0600

    Changed exp body to reach text

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -71,6 +71,6 @@ class ExperiencesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def experience_params
-      params.require(:experience).permit(:title, :organization, :body, :date_started, :date_ended, :location)
+      params.require(:experience).permit(:title, :organization, :exp_body, :date_started, :date_ended, :location)
     end
 end
