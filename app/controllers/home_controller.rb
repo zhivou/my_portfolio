9e7b4ee2bc6518d3826e5b963463cfdf62a33618
Dmitrii Skrylev <dima.skrylev@plansource.com>
2019-07-19 10:57:05 -0600

    Removed api, decided to go with simple Rails partial

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @latest_blogs = Blog.order(id: :desc).limit(6)
+    @latest_projects = Project.order(id: :desc).limit(6)
   end
 end
--- app/javascript/packs/HomeProjects.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './projects/App'
-import 'semantic-ui-css/semantic.min.css';
-
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-      <App name="React" />, document.getElementById('reactProjects'))
-});
