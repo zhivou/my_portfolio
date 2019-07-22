99d73fc8ef023b0d2fec51f1b2d0539c47d61102
Dmitrii Skrylev <dima.skrylev@plansource.com>
2019-07-22 16:50:36 -0600

    Get read of wrong active class toggle

--- app/javascript/packs/other/nav_bar_actions.js
+++ app/javascript/packs/other/nav_bar_actions.js
@@ -3,8 +3,3 @@ var $home = $('#button-home');
 $home.click(function() {
   //do something
 });
-
-$( 'li span' ).on( 'click', function () {
-  $( 'li span' ).find( 'span.active' ).removeClass( 'active' );
-  $( this ).parent( 'span' ).addClass( 'active' );
-});
