327fe57861e146f322108b5fe3d8229549322026
zhivou <zivou4@gmail.com>
2020-01-06 00:07:38 -0700

    Qucik fixes

--- app/javascript/packs/photos/Spinner.jsx
+++ app/javascript/packs/photos/Spinner.jsx
@@ -4,9 +4,9 @@ import ReactDOM from 'react-dom';
 class Spinner extends React.Component {
   render() {
     return (
-      <div class="d-flex justify-content-center">
-        <div class="spinner-border" role="status" id="spinner">
-          <span class="sr-only">Loading...</span>
+      <div className="d-flex justify-content-center">
+        <div className="spinner-border" role="status" id="spinner">
+          <span className="sr-only">Loading...</span>
         </div>
       </div>
     );
