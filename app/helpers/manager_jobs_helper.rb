487dac3ebd9dbf284bb523667b4c9a3c86827ed6
zhivou <zivou4@gmail.com>
2020-03-15 12:11:03 -0600

    Added Interview helper and badge renders

--- app/helpers/manager_jobs_helper.rb
+++ app/helpers/manager_jobs_helper.rb
@@ -1,2 +1,25 @@
 module ManagerJobsHelper
+
+  ##
+  # Expects job_element. Renders pending/expired/successful icon for
+  # job index page cards.
+  #
+  def pending_interview(job_element)
+    date_created =  job_element.created_at
+    date_today = Date.today
+    invited_for_interview = job_element.interview
+    id = job_element.id
+
+    case
+    when date_created + 30.days < date_today && !invited_for_interview
+      # Expired
+      "<span class='badge badge-danger'>Expired</span>".html_safe
+    when invited_for_interview
+      # Interview Happened
+      "<span class='badge badge-success'>Invited</span>".html_safe
+    else
+      # Pending
+      "<span class='badge badge-primary'>Pending</span>".html_safe
+    end
+  end
 end
