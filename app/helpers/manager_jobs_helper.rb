26b3500b7d811bdbf3eff06df30a7d78c5729217
zhivou <zivou4@gmail.com>
2020-03-15 12:34:57 -0600

    Added pending heper for replied field

--- app/helpers/manager_jobs_helper.rb
+++ app/helpers/manager_jobs_helper.rb
@@ -1,17 +1,17 @@
 module ManagerJobsHelper
 
+  DATE_TODAY = Date.today
+
   ##
   # Expects job_element. Renders pending/expired/successful icon for
   # job index page cards.
   #
   def pending_interview(job_element)
     date_created =  job_element.created_at
-    date_today = Date.today
     invited_for_interview = job_element.interview
-    id = job_element.id
 
     case
-    when date_created + 30.days < date_today && !invited_for_interview
+    when date_created + 30.days < DATE_TODAY && !invited_for_interview
       # Expired
       "<span class='badge badge-danger'>Expired</span>".html_safe
     when invited_for_interview
@@ -20,6 +20,18 @@ module ManagerJobsHelper
     else
       # Pending
       "<span class='badge badge-primary'>Pending</span>".html_safe
+    end
+  end
+
+  ##
+  # Translates boolean to yes or no
+  # for replay field
+  #
+  def pending_reply(job_element)
+    if job_element.replied
+      "<span class='badge badge-success'>Yes</span>".html_safe
+    else
+      "<span class='badge badge-danger'>No</span>".html_safe
     end
   end
 end
