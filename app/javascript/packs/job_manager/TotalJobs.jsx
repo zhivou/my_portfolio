38d0f9fea048074df114271d8fe95d3e4d3b6cad
zhivou <zivou4@gmail.com>
2020-03-16 14:16:47 -0600

    Creted Successful chart

--- app/javascript/packs/job_manager/TotalJobs.jsx
+++ app/javascript/packs/job_manager/TotalJobs.jsx
@@ -9,41 +9,81 @@ class TotalJobs extends React.Component {
       jobs: gon.jobs
     };
 
+    this.loadTotalJobs = this.loadTotalJobs.bind(this);
+
   }
 
-  componentDidMount() {
-    this.setState({
-      jobs: gon.jobs,
-    });
+  loadTotalJobs(){
+    const jobs = this.state.jobs;
+    return (
+      <div className="chartItem">
+        <Doughnut
+          chartData={
+            {
+              labels: ['Jobs'],
+              datasets: [{
+                label: '# of Votes',
+                data: [jobs.length ? jobs.length : 1],
+                backgroundColor: [
+                  'rgba(25,54,65)',
+                  'rgba(255, 255, 255, 0)'],
+                borderColor: [
+                  'rgba(25,54,65)',
+                  'rgba(255, 99, 132, 1)'],
+                borderWidth: 10}]
+            }
+          }
+          displayLegend={false}
+          chartName='Total Jobs:'
+        />
+        <h3 className="total-count">{jobs.length < 10 ? '0' + `${jobs.length}` : jobs.length}</h3>
+      </div>
+    )
+  }
+
+  loadSuccessfulJobs(){
+    const jobs = this.state.jobs;
+    const successfulJobs = jobs.filter(job => job.interview && job.replied);
+
+    return (
+      <div className="chartItem">
+        <Doughnut
+          chartData={
+            {
+              labels: ['Successful Jobs'],
+              datasets: [{
+                label: '# of Votes',
+                data: [Array.isArray(successfulJobs) ? successfulJobs.length : 1],
+                backgroundColor: [
+                  'rgba(25,54,65)',
+                  'rgba(255, 255, 255, 0)'],
+                borderColor: [
+                  'rgba(25,54,65)',
+                  'rgba(255, 99, 132, 1)'],
+                borderWidth: 10}]
+            }
+          }
+          displayLegend={false}
+          chartName='Successful Jobs:'
+        />
+        <h3 className="total-count">{successfulJobs.length < 10 ? '0' + `${successfulJobs.length}` : successfulJobs.length}</h3>
+      </div>
+    )
+  }
+
+  initChart(){
+    return(
+      <div>
+        a
+      </div>
+    )
   }
 
   render() {
-    const jobs = this.state.jobs;
-
     return (
       <div>
-        <div className="chartItem">
-          <Doughnut
-            chartData={
-              {
-                labels: ['Jobs'],
-                datasets: [{
-                  label: '# of Votes',
-                  data: [jobs.length],
-                  backgroundColor: [
-                    'rgba(25,54,65)',
-                    'rgba(255, 255, 255, 0)'],
-                  borderColor: [
-                    'rgba(25,54,65)',
-                    'rgba(255, 99, 132, 1)'],
-                  borderWidth: 10}]
-              }
-            }
-            displayLegend={false}
-            chartName='Total Jobs:'
-          />
-          <h3 className="total-count">{jobs.length}</h3>
-        </div>
+        {this.loadTotalJobs()}
+        {this.loadSuccessfulJobs()}
       </div>
     );
   }
