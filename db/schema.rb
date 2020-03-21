3241b3732b77447c884db7d50b1173bb8c8c71ea
zhivou <zivou4@gmail.com>
2020-03-21 16:50:36 -0600

    Migrated Stocks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_21_222135) do
+ActiveRecord::Schema.define(version: 2020_03_21_223901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -143,6 +143,18 @@ ActiveRecord::Schema.define(version: 2020_03_21_222135) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.boolean "current"
+    t.text "notes"
+    t.date "sold_date"
+    t.decimal "sold_price"
+    t.decimal "gain_loss"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "tags", force: :cascade do |t|
