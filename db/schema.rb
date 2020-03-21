224df03dd5aace3d56e3a0e039b6ac44b5ded5ed
zhivou <zivou4@gmail.com>
2020-03-21 17:31:06 -0600

    Migrated FinancialTyps table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_21_225308) do
+ActiveRecord::Schema.define(version: 2020_03_21_232640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,12 @@ ActiveRecord::Schema.define(version: 2020_03_21_225308) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "sort"
+  end
+
+  create_table "financial_types", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "incomes", force: :cascade do |t|
