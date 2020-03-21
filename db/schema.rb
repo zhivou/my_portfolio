19eb82c9bc0ee8458ebca92adfc124eeca13d8d8
zhivou <zivou4@gmail.com>
2020-03-21 16:56:58 -0600

    Migrated Expenses table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_21_223901) do
+ActiveRecord::Schema.define(version: 2020_03_21_225308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,16 @@ ActiveRecord::Schema.define(version: 2020_03_21_223901) do
     t.date "date_ended"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "expenses", force: :cascade do |t|
+    t.string "name"
+    t.decimal "monthly_payment"
+    t.boolean "current"
+    t.text "notes"
+    t.decimal "year_amount"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "experiences", force: :cascade do |t|
