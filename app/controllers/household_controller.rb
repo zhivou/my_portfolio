4bc00a56b912fbfba42519a37b666f3de2ac6755
zhivou <zivou4@gmail.com>
2020-03-21 23:20:33 -0600

    Added total monthly income calulation

--- app/controllers/household_controller.rb
+++ app/controllers/household_controller.rb
@@ -9,17 +9,24 @@ class HouseholdController < ApplicationController
     incomes = Income.includes(:financial_type).all
     loans = Loan.includes(:financial_type).all
     stocks = Stock.includes(:financial_type).all
-    all_expenses = get_all_expenses(expenses, loans)
+    all_expenses = get_all_monthly_expenses(expenses, loans)
+    all_income = get_all_monthly_income(incomes)
 
     gon.expenses = expenses
     gon.incomes = incomes
     gon.loans = loans
     gon.stocks = stocks
-    gon.get_all_monthly_expenses = all_expenses
   end
 
   private
   def get_all_monthly_expenses(expenses, loans)
     gon.totalMonthlyExpenses = expenses.total_by_month + loans.total_by_month
+  end
+
+  ##
+  # TODO: Add dividends later when stock api is in place
+  #
+  def get_all_monthly_income(incomes)
+    gon.totalMonthlyIncome = incomes.total_by_month
   end
 end
