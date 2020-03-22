72e6c912d58f38dc728682734e31d02f7130c487
zhivou <zivou4@gmail.com>
2020-03-21 23:11:56 -0600

    Added all monthly counter

--- app/controllers/household_controller.rb
+++ app/controllers/household_controller.rb
@@ -5,9 +5,21 @@ class HouseholdController < ApplicationController
   end
 
   def budget
-    gon.expenses = Expense.includes(:financial_type).all
-    gon.incomes = Income.includes(:financial_type).all
-    gon.loans = Loan.includes(:financial_type).all
-    gon.stocks = Stock.includes(:financial_type).all
+    expenses = Expense.includes(:financial_type).all
+    incomes = Income.includes(:financial_type).all
+    loans = Loan.includes(:financial_type).all
+    stocks = Stock.includes(:financial_type).all
+    all_expenses = get_all_expenses(expenses, loans)
+
+    gon.expenses = expenses
+    gon.incomes = incomes
+    gon.loans = loans
+    gon.stocks = stocks
+    gon.get_all_monthly_expenses = all_expenses
+  end
+
+  private
+  def get_all_monthly_expenses(expenses, loans)
+    gon.totalMonthlyExpenses = expenses.total_by_month + loans.total_by_month
   end
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,8 @@
 class Expense < ApplicationRecord
   belongs_to :financial_type
+  scope :current, -> { where(current: true) }
+
+  def self.total_by_month
+    current.sum(:monthly_payment)
+  end
 end
