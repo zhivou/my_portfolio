ee5fadad17a14d0f78b80f0475815869db23bd2b
zhivou <zivou4@gmail.com>
2020-03-27 00:07:01 -0600

    Rendered income table

--- app/javascript/packs/budget_manager/App.jsx
+++ app/javascript/packs/budget_manager/App.jsx
@@ -96,6 +96,47 @@ class App extends React.Component {
     )
   }
 
+  initIncomeTable(){
+    const incomes = this.state.incomes;
+
+    return(
+      <div className="p-4 m-4">
+        <section className="panel">
+          <header className="panel-heading">
+            Income
+          </header>
+          <table className="table table-hover">
+            <thead>
+            <tr>
+              <th>#</th>
+              <th>Source Name</th>
+              <th>Monthly Income</th>
+              <th>Year Income</th>
+              <th>Notes</th>
+              <th>Type</th>
+            </tr>
+            </thead>
+            <tbody>
+
+            {incomes.map(inc => (
+              <tr key={inc.id}>
+                <td>{inc.id}</td>
+                <td>{inc.source_name}</td>
+                <td>{inc.monthly_income}</td>
+                <td>{inc.year_income}</td>
+                <td>{inc.notes}</td>
+                <td>{inc.name}</td>
+              </tr>
+            ))
+            }
+
+            </tbody>
+          </table>
+        </section>
+      </div>
+    )
+  }
+
   render() {
     const state = this.state;
     return (
@@ -112,6 +153,11 @@ class App extends React.Component {
           </div>
           <div className="col-md-3">
             {this.initLinksCard()}
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 ">
+            {this.initIncomeTable()}
           </div>
         </div>
         <div className="row">
