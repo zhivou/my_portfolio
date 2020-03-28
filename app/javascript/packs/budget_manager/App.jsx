9159f4e890545a3288253dcbb6244c5535327aac
zhivou <zivou4@gmail.com>
2020-03-27 19:20:29 -0600

    Finished first Income pie

--- app/javascript/packs/budget_manager/App.jsx
+++ app/javascript/packs/budget_manager/App.jsx
@@ -1,6 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GoogleCalendar from "./GoogleCalendar";
+import PieChart from './PieChart'
+
+const backgroundColor = [
+  '#197278',
+  '#50514F',
+  '#F25F5C',
+  '#FFE066',
+  '#70C1B3',
+  '#0E3F42',
+  '#845A6D',
+  '#6E75A8',
+  '#8D91C7',
+  '#B0DAF1',
+  '#0E3F42',
+  '#F7DBA7',
+  '#F1AB86',
+  '#C57B57',
+  '#ECFEE8',
+  '#197278',
+  '#50514F',
+  '#F25F5C',
+  '#FFE066',
+  '#70C1B3',
+  '#0E3F42',
+  '#845A6D',
+];
+
+const icons = {
+  thumbUp: 'fa fa-thumbs-o-up',
+  plus: 'fa fa-plus',
+  lines: 'fa fa-align-left',
+  exclamation: 'fa fa-exclamation-triangle',
+  leaf: 'fa fa-envira'
+};
 
 class App extends React.Component {
   constructor(props) {
@@ -16,14 +50,6 @@ class App extends React.Component {
       dti: gon.dti,
       totalExpenses: gon.totalExpenses
     };
-
-    this.icons = {
-      thumbUp: 'fa fa-thumbs-o-up',
-      plus: 'fa fa-plus',
-      lines: 'fa fa-align-left',
-      exclamation: 'fa fa-exclamation-triangle',
-      leaf: 'fa fa-envira'
-    }
   }
 
   initCards(count, title, color, icon) {
@@ -242,15 +268,55 @@ class App extends React.Component {
     )
   }
 
+  createIncomePie(){
+    const data = [];
+    const labels = [];
+    let total = 0;
+
+    this.state.incomes.forEach(function (item) {
+      data.push(parseFloat(item.monthly_income));
+      labels.push(item.source_name);
+      total = total + parseFloat(item.monthly_income);
+    });
+
+    return(
+      this.initChart(labels, data, total, 'Income Pie', total)
+    )
+  }
+
+  initChart(labels, data, dataOffset, chartName, total){
+    return(
+      <div className="card m-2 p-2 pt-5 chartItem">
+        <PieChart
+          chartData={
+            {
+              labels: labels,
+              datasets: [{
+                label: '# of Votes',
+                data: data,
+                backgroundColor: backgroundColor,
+                borderColor: [],
+                borderWidth: 0}]
+            }
+          }
+          displayLegend={false}
+          chartName={chartName}
+          fatSize='100%'
+        />
+        <h3 className="text-center m-2">{total}</h3>
+      </div>
+    )
+  }
+
   render() {
     const state = this.state;
     return (
       <div>
         <div className="row">
-          {this.initCards(`$${state.allMonthlyExpenses}`, 'Total Expenses', 'red-bg', this.icons.lines)}
-          {this.initCards(state.allMonthlyIncome, 'Income', 'green-bg', this.icons.plus)}
-          {this.initCards(state.totalDept, 'Dept', 'dark-bg', this.icons.exclamation)}
-          {this.initCards(state.dti, 'DTI', 'blue-bg', this.icons.leaf)}
+          {this.initCards(`$${state.allMonthlyExpenses}`, 'Total Expenses', 'red-bg', icons.lines)}
+          {this.initCards(state.allMonthlyIncome, 'Income', 'green-bg', icons.plus)}
+          {this.initCards(state.totalDept, 'Dept', 'dark-bg', icons.exclamation)}
+          {this.initCards(state.dti, 'DTI', 'blue-bg', icons.leaf)}
         </div>
         <div className="row">
           <div className="col-lg-9 col-md-12">
@@ -260,6 +326,15 @@ class App extends React.Component {
             {this.initLinksCard()}
           </div>
         </div>
+
+
+        <div className="row">
+          <div className="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+            {this.createIncomePie()}
+          </div>
+        </div>
+
+
         <div className="row">
           <div className="col-12 ">
             {this.initIncomeTable()}
