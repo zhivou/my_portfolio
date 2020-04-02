8feb8c76695a636870cc51b87d5d948016f9a497
zhivou <zivou4@gmail.com>
2020-04-02 00:23:01 -0600

    Rendered bar Investment chart

--- app/javascript/packs/stock_manager/App.jsx
+++ app/javascript/packs/stock_manager/App.jsx
@@ -1,31 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ChartComponent from './Chart'
+
+const chartData = {
+  labels: [],
+
+  datasets:[
+    {
+      label:'Current',
+      data: [gon.totalCurrentInvestments],
+      backgroundColor: "rgba(0, 177, 106, 1)"
+    },
+    {
+      label:'Initial',
+      data: [gon.totalOriginalInvestments],
+      backgroundColor: "rgba(231, 76, 60, 1)"
+    },
+  ]
+
+};
+
+const step = Math.abs((gon.totalCurrentInvestments - gon.totalOriginalInvestments) * 4);
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      chartData: []
+      chartData: chartData
     };
   }
 
-  componentDidMount() {
+  initTotalInvestmentsCard() {
+    return (
+      <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+        <div className="card">
+          <div className="card-header">
+            Current Investments
+          </div>
+          <div className="card-body">
+            <ChartComponent
+              chartData={this.state.chartData}
+              chartName="Investing"
+              legendPosition="bottom"
+              stepSize={step}
+            />
+          </div>
+        </div>
+      </div>
+    )
   }
 
   render() {
     return (
       <div>
         <div className="row">
-          <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-            <div className="card">
-              <div className="card-header">
-                Current Investments
-              </div>
-              <div className="card-body">
-                <h4 className="card-title">Special title treatment</h4>
-              </div>
-            </div>
-          </div>
+          {this.initTotalInvestmentsCard()}
         </div>
       </div>
     );
