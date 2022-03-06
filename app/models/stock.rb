57b8b8523f8bf19fd0bdf2aceb57773fd99339d0
zhivou <zivou4@gmail.com>
2022-03-06 18:25:53 -0500

    Add stock  client check

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -31,12 +31,14 @@ class Stock < ApplicationRecord
     result = 0
 
     names.each do |n|
-      if Time.now > $stock_prices[:time] + 30.minutes || !$stock_prices[n]
-        $stock_prices[n] = CLIENT.quote(n).latest_price.to_f
-        $stock_prices[:time] = Time.now
-        $stock_prices[:will_update_at] = Time.now + 30.minutes
+      if CLIENT.secret_token
+        if Time.now > $stock_prices[:time] + 30.minutes || !$stock_prices[n]
+          $stock_prices[n] = CLIENT.quote(n).latest_price.to_f
+          $stock_prices[:time] = Time.now
+          $stock_prices[:will_update_at] = Time.now + 30.minutes
+        end
+        result += stocks.where(name: n).count * $stock_prices[n]
       end
-      result += stocks.where(name: n).count * $stock_prices[n]
     end
 
     result
@@ -65,6 +67,7 @@ class Stock < ApplicationRecord
   end
 
   def self.create_price_holder(n)
+    return unless CLIENT.secret_token
     holder = {}
 
     holder['quote'] = CLIENT.quote(n)
