cac50a1aa6f21fe9dd2990c4e46de19d6d93cc82
zhivou <zivou4@gmail.com>
2022-03-16 23:31:45 -0400

    More work to tables logic

--- db/WIP/20220307020922_create_x_tables.rb
+++ db/WIP/20220307020922_create_x_tables.rb
@@ -1,32 +1,38 @@
 class CreateXTables < ActiveRecord::Migration[6.1]
   def change
+    ##
+    # Positions are represents users portfolio. It can contain
+    # multiple items: stocks(stocks or ETFs, index funds) or Cryptos.
+    create_table    :x_positions do |t|
+      t.references  :user, null: false, foreign_key: true
+
+      t.references  :x_stocks, null: true, foreign_key: true
+      t.references  :x_cryptos, null: true, foreign_key: true
+
+      t.decimal     :shares, precision: 12, scale: 4
+      t.decimal     :average_price, precision: 10, scale: 2
+      t.decimal     :total_cost, precision: 10, scale: 2
+      t.decimal     :market_price, precision: 10, scale: 2
+      t.decimal     :market_value, precision: 10, scale: 2
+      t.decimal     :gain_loss, precision: 10, scale: 2
+      t.decimal     :gain_loss_pct, precision: 10, scale: 2
+      t.decimal     :est_annual_dividend, precision: 12, scale: 4
+      t.decimal     :est_annual_income, precision: 12, scale: 4
+      t.string      :note
+
+      t.boolean     :current
+      t.timestamps
+
+      t.index %i[user_id stock_id], unique: true
+    end
+
+
     create_table    :x_stocks do |t|
       t.string      :symbol, null: false
 
-      t.references  :brokers, foreign_key: true
+      t.references  :x_brokers, foreign_key: true
       t.references  :x_companies, foreign_key: true
       t.references  :x_dividends, foreign_key: true
-
-      t.string      :company_name
-      t.string      :industry
-      t.string      :website
-      t.text        :description
-      t.string      :ceo
-      t.string      :security_name
-      t.string      :issue_type
-      t.string      :sector
-      t.integer     :primary_sic_code
-      t.integer     :employees
-      t.string      :address
-      t.string      :address2
-      t.string      :state
-      t.string      :city
-      t.string      :zip
-      t.string      :country
-      t.string      :phone
-      t.date        :ipo
-      t.string      :logo
-      t.string      :peers
 
       t.decimal     :outstanding_shares, precision: 16, scale: 0
       t.decimal     :market_capitalization, precision: 20, scale: 0
@@ -47,6 +53,37 @@ class CreateXTables < ActiveRecord::Migration[6.1]
       t.timestamps
     end
 
+    ##
+    # USE coinpaprika
+    # https://api.coinpaprika.com/#tag/Global/paths/~1global/get
+    create_table    :x_cryptos do |t|
+      t.string      :symbol, null: false
+
+      t.references  :x_crypto_projects, foreign_key: true
+
+      t.decimal     :current_price, precision: 10, scale: 2
+      t.decimal     :volume, precision: 10, scale: 2
+      t.integer     :rank
+      t.integer     :total_supply
+      t.integer     :circulating_supply
+      t.integer     :max_supply
+      t.decimal     :market_cap, precision: 10, scale: 2
+      t.decimal     :price_1h_percentage_change, precision: 10, scale: 2
+      t.decimal     :price_24h_percentage_change, precision: 10, scale: 2
+      t.decimal     :price_7d_percentage_change, precision: 10, scale: 2
+      t.decimal     :price_30d_percentage_change, precision: 10, scale: 2
+      t.decimal     :price_3m_percentage_change, precision: 10, scale: 2
+      t.decimal     :price_1y_percentage_change, precision: 10, scale: 2
+      t.decimal     :price_5y_percentage_change, precision: 10, scale: 2
+      t.decimal     :all_time_high_price, precision: 10, scale: 2
+      t.decimal     :all_time_high_percentage_drop, precision: 10, scale: 2
+      t.decimal     :all_time_high_date, precision: 10, scale: 2
+      t.decimal     :untrusted_volume, precision: 10, scale: 2
+    end
+
+    ##
+    # https://iexcloud.io/docs/api/#company
+    # GET /stock/{symbol}/company
     create_table    :x_companis do |t|
       t.string      :name
       t.string      :exchange
@@ -83,8 +120,14 @@ class CreateXTables < ActiveRecord::Migration[6.1]
       t.string      :organization_structure
       t.string      :development_status
       t.string      :github
+
+      t.boolean     :current
+      t.timestamps
     end
 
+    ##
+    # https://iexcloud.io/docs/api/#dividends-basic
+    # GET /stock/{symbol}/dividends/{range}
     create_table    :x_dividends do |t|
       t.decimal     :amount, precision: 10, scale: 2
       t.string      :currency
@@ -106,55 +149,13 @@ class CreateXTables < ActiveRecord::Migration[6.1]
       t.timestamps
     end
 
-    create_table    :brokers do |t|
+    create_table    :x_brokers do |t|
       t.string      :name, null: false
       t.string      :location_url
       t.string      :credentials
       t.text        :notes
 
       t.datetime    :created_at, null: false
-    end
-
-    create_table    :positions do |t|
-      t.references  :user, null: false, foreign_key: true
-      t.references  :x_stock, null: true, foreign_key: true
-      t.references  :x_crypto, null: true, foreign_key: true
-
-      t.decimal     :shares, precision: 12, scale: 4
-      t.decimal     :average_price, precision: 10, scale: 2
-      t.decimal     :total_cost, precision: 10, scale: 2
-      t.decimal     :market_price, precision: 10, scale: 2
-      t.decimal     :market_value, precision: 10, scale: 2
-      t.decimal     :gain_loss, precision: 10, scale: 2
-      t.decimal     :gain_loss_pct, precision: 10, scale: 2
-      t.decimal     :est_annual_dividend, precision: 12, scale: 4
-      t.decimal     :est_annual_income, precision: 12, scale: 4
-      t.string      :note
-
-      t.index %i[user_id stock_id], unique: true
-    end
-
-    create_table    :x_cryptos do |t|
-      t.string      :name
-      t.string      :symbol
-      t.decimal     :price, precision: 10, scale: 2
-      t.decimal     :volume, precision: 10, scale: 2
-      t.integer     :rank
-      t.integer     :total_supply
-      t.integer     :circulating_supply
-      t.integer     :max_supply
-      t.decimal     :market_cap, precision: 10, scale: 2
-      t.decimal     :price_1h_percentage_change, precision: 10, scale: 2
-      t.decimal     :price_24h_percentage_change, precision: 10, scale: 2
-      t.decimal     :price_7d_percentage_change, precision: 10, scale: 2
-      t.decimal     :price_30d_percentage_change, precision: 10, scale: 2
-      t.decimal     :price_3m_percentage_change, precision: 10, scale: 2
-      t.decimal     :price_1y_percentage_change, precision: 10, scale: 2
-      t.decimal     :price_5y_percentage_change, precision: 10, scale: 2
-      t.decimal     :all_time_high_price, precision: 10, scale: 2
-      t.decimal     :all_time_high_percentage_drop, precision: 10, scale: 2
-      t.decimal     :all_time_high_date, precision: 10, scale: 2
-      t.decimal     :untrusted_volume, precision: 10, scale: 2
     end
   end
 end
