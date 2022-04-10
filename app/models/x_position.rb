1bc8ca99f0bbec9e3b082f87a73ce2bc4a4c5c3d
zhivou <zivou4@gmail.com>
2022-04-10 00:10:03 -0400

    Fix validation for Position Model

--- app/models/x_position.rb
+++ app/models/x_position.rb
@@ -1,6 +1,9 @@
 class XPosition < ApplicationRecord
-  belongs_to :x_stock
-  belongs_to :x_crypto
+  belongs_to :x_stock, optional: true
+  belongs_to :x_crypto, optional: true
+
+  validates_associated :x_stock
+  validates_associated :x_crypto
 
   scope :current, -> { where(current: true) }
 end
