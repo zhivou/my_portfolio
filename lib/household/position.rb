e82af78dd0a112d4400979e7798e58fbc7470280
zhivou <zivou4@gmail.com>
2022-04-11 22:47:44 -0400

    Yield calc for dividens

--- lib/household/position.rb
+++ lib/household/position.rb
@@ -1,6 +1,13 @@
 
 module Household
   class Position
+    FREQUENCY = {
+      every_35: 10.4,
+      annual: 1,
+      quarterly: 4,
+      monthly: 12
+    }
+
     def self.gain_loss(position)
       market_value = (position.shares * position.x_stock.current_price)
       purchased_value = position.total_cost
@@ -14,17 +21,22 @@ module Household
       ((total * 100) / purchased_value).round(2)
     end
 
-    # TODO: Revise this one
-    def self.estimate_div(position)
+    def self.yield_div(position)
       shares = position.shares
 
       if position.x_stock.dividends.exists?
         dividends = position.x_stock.dividends.last
+        frequency = dividends.frequency
+        price = position.x_stock.current_price
 
-        shares * dividends.amount
-      else
-        '---'
+        (((dividends.amount * FREQUENCY[frequency.to_sym]) / price) * 100).round(2)
       end
+    end
+
+    def self.estimated_income(position)
+      div_prc = self.yield_div(position)
+      market_value = position.shares * position.x_stock.current_price
+      (market_value * (div_prc / 100 )).round(2) unless div_prc.nil?
     end
   end
 end
